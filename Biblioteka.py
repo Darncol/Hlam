e8e6f9c3c19b94e307fd9a27e37f1d3327d3e567
Darncol <60666804+Darncol@users.noreply.github.com>
2020-02-11 21:48:51 +0200

    Add files via upload

--- Biblioteka.py
+++ Biblioteka.py
@@ -1,28 +1,37 @@
+import os
+# os.system("clear")
+
 def zapisatknigu():
+    os.system("clear")
     try:
-        newknigaN = input("nazvanie:")
-        newknigaC = int(input("Cena:"))
+        newknigaN = input("Название книги:")
+        newknigaC = int(input("Цена:"))
+        newknigaA = "Green"
         if newknigaC >= 0:
             f = open("Biblioteka.txt", "a")
-            gotstr = newknigaN + ".," + str(newknigaC) + "\n"
+            gotstr = newknigaN + ".," + str(newknigaC) + ".," + newknigaA + "\n"
             f.write(gotstr)
-            print("zapisano\n")
+            print("Записано\n")
             f.close()
     except:
-        print("Cena ne pravilnaja baran\n")
+        print("Цена не правильная\n")
 
 def pokazatknigi():
     spisokknig = vernutspisok()
     spisokknig = "\n".join(spisokknig)
-    spisokknig = spisokknig.replace(".,"," ")
-    print("Vse knigi:\n" + spisokknig  + "\n")
+    spisokknig = spisokknig.replace(".,", " ")
+    spisokknig = spisokknig.replace("Green", "Есть в наличии")
+    spisokknig = spisokknig.replace("Red", "Нет в наличии")
+    print("Все книги:\n" + spisokknig  + "\n")
     return
 
 def pokazatcenu():
+    os.system("clear")
     try:
+
         spisokknig = vernutspisok()
 
-        nazvknigi = input("Nazvanie: ")
+        nazvknigi = input("Название книги: ")
 
         knigi = []
         ceni = []
@@ -37,8 +46,11 @@ def pokazatcenu():
             takniga = knigi.index(nazvknigi)
             print(str(ceni[takniga])+"\n")
 
+        elif nazvknigi != knigi:
+            print("Нету книги.\n")
+
     except:
-        print("Knigi netu\n")
+        print("Error\n")
 
     return
 
@@ -53,22 +65,29 @@ def vernutspisok():
 
     return verper
 
+def vernutimena():
+    f = open('Bibli_Name.txt')
+    imena = f.read().splitlines()
+    f.close()
+    return imena
 
 def dobavitdeneg():
+    os.system("clear")
     try :
-        lave = int(input("Skolko deneg dobavit?:"))
+        lave = int(input("Сколько денег добавить?:"))
         if lave >= 0:
             global kowel
             kowel = kowel + lave
             print("\n")
     except:
-        print("Vse huina davai po novoi\n\n")
+        print("Error\n\n")
 
 def kupitknigu():
+    os.system("clear")
     try:
         spisokknig = vernutspisok()
 
-        nazvknigi = input("Kakuju knigu kupit?: ")
+        nazvknigi = input("Какую книгу купить?: ")
 
         knigi = []
         ceni = []
@@ -83,29 +102,145 @@ def kupitknigu():
             takniga = knigi.index(nazvknigi)
             cena = ceni[takniga]
 
-            if kowel - cena > 0:
+            if kowel - cena >= 0:
                 kowel = kowel - cena
 
             else:
-                print("Netu deneg baran!\n")
+                print("Нхватает денег\n")
 
 
         else:
-            print("knigi netu\n")
+            print("Книги нету\n")
 
 
     except:
-        print("Knigi netu\n")
+        print("Error\n")
 
     return
+
+def arendaknig():
+    os.system("clear")
+    vibor = input("""Привет,что хотите зделать?
+    1 = Взять книгу в аренду
+    2 = Вернуть книгу
+    3 = Проверить не возвращенные книги
+    4 = Зарегистрироваться
+    >""")
+
+    if vibor == "1":
+        os.system("clear")
+        f = open("Bibli_Name.txt")
+        vname = f.read().splitlines()
+        f.close()
+        # print(vname)
+
+        imja = []
+        razrewenie = []
+        nazvanievzatoiknigi = []
+        dlazapis = []
+
+        try:
+            for stroka in vname:
+                stroka = stroka.split(".,")
+                imja.append(stroka[0])
+                razrewenie.append(stroka[1])
+                nazvanievzatoiknigi.append(stroka[2])
+                dlazapis = dlazapis.append(stroka)
+                print(stroka)
+        except:
+            print("Error")
+
+        name = input("Как вас зовут?: ")
+
+        if name in imja:
+            inname = imja.index(name)
+
+            if razrewenie[inname] == 'Allow':
+                knigavarendu = input('1 = Выбрать книгу?\n2 = показать список?\n>')
+
+                if knigavarendu == '1':
+                    nazvknigi = input('Название книги?: ')
+
+                    spisokknig = vernutspisok()
+                    knigi = []
+                    ceni = []
+                    dostup = []
+
+                    for stroka2 in spisokknig:
+                        stroka2 = stroka2.split('.,')
+                        knigi.append(stroka2[0])
+                        ceni.append(stroka2[1])
+                        dostup.append(stroka2[2])
+
+                    if nazvknigi in knigi:
+                        nomerknigi = knigi.index(nazvknigi)
+                        print(nomerknigi)
+
+                        if dostup[nomerknigi] == 'Green':
+
+                            dlazapis[nomerknigi[2]] = "Denied"
+                            print(dlazapis)
+
+
+
+                            zapis = open('Bibli_Name.txt', 'w')
+                            zapis.write(dlazapis)
+                            zapis.close()
+                            
+
+                        elif dostup[nomerknigi] == 'Denied':
+                            os.system('clear')
+                            print('Книга уже в аренде\n')
+
+                        else:
+                            os.system('clear')
+                            print('Проверь название\n')
+
+                elif knigavarendu == '2':
+                    pokazatknigi()
+
+                else:
+                    os.system('clear')
+                    print('Erorr434')
+
+            elif razrewenie[inname] == 'Denied':
+                print('Нужно вернуть старые книги\n')
+        else:
+            print('Нет такого имени в базе\n')
+
+    elif vibor == "2":
+        pass
+
+    elif vibor == "3":
+        knigi = vernutspisok()
+
+    elif vibor == "2":
+        pass
+
+    elif vibor == "3":
+        pass
+
+    elif vibor == "4":
+
+        name = input("Как вас зовут?: ")
+
+        nname = name + ".," + "Allow" + ".," + "pusto" + "\n"
+
+        f = open("Bibli_Name.txt","a")
+        zapis = f.write(nname)
+
+        os.system("clear")
+        print("Записано")
+
 
 
 kowel = 0
 
 def main():
     while True:
-        print("Deneg$$$ =", kowel)
-        vvod = input("1 = zapisat knigu\n2 = pokazat knigi\n3 = pokazat cenu\n4 = dobavit deneg\n5 = kupit knigu\n9 = vihod\n> ")
+
+        print("Денег$$$ =", kowel)
+        vvod = input("1 = Добавить книгу\n2 = Показать все книги\n3 = Показать цену книги\n4 = Добавить денег\n5 = Купить книгу\n6 = Аренда книг\n9 = Выход\n> ")
 
         if vvod == "1":
             zapisatknigu()
@@ -122,13 +257,16 @@ def main():
         elif vvod == "5":
             kupitknigu()
 
+        elif vvod == "6":
+            arendaknig()
+
 
         elif vvod == "9":
             print("exit")
             break
 
         else :
-            print("naher")
+            print("Не балуйся!")
 
 if __name__ == "__main__":
     main()
