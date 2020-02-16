c6d492a3f9d61778613ee3b81fef72c0373d1156
Darncol <60666804+Darncol@users.noreply.github.com>
2020-02-16 22:03:22 +0200

    Add files via upload

---
+++ Crypto.py
@@ -0,0 +1,63 @@
+import os
+
+def crypt():
+    beg_cry = input('Текст для шифрования:')
+    bukvi = list(beg_cry)
+    wifrovanie = []
+    razwifrovanie = []
+    
+    for bukva in bukvi:
+        a = bukva
+        b = ord(a)
+        c = b + 7
+        wifrovanie.append(c)
+
+    for bukva1 in wifrovanie:
+        a = bukva1
+        b = chr(a)
+        razwifrovanie.append(b)
+    
+    final_txt = ''.join(razwifrovanie)
+    
+    f = open('Test.txt','w')
+    f.write(final_txt)
+    f.close
+
+    return final_txt
+
+def uncrypt():
+    wifr = []
+    un_wifr = []
+    fin_wifr = []
+
+    f = open('Test.txt')
+    a = f.read()
+    wifr = list(a)
+    f.close
+
+    for bukva in wifr:
+        a = bukva
+        a = ord(a)
+        b = a - 7
+        un_wifr.append(b)
+
+    for bukva1 in un_wifr:
+        a = bukva1
+        a = chr(a)
+        fin_wifr.append(a)
+    
+    print(''.join(fin_wifr))
+
+    return
+
+os.system('clear')
+vvod = input('1.Зашифровать\n2.Расшифровать\n>')
+
+if vvod == "1":
+    print(crypt())
+
+elif vvod == "2":
+    uncrypt()
+
+else:
+    print('Что-то пошло не так!')
